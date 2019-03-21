caf5403b9702d057b37a1595ecad1d15c66050ff
zhaoruinan <zhaoruinan2010@gmail.com>
2019-03-22 00:15:08 +0900

    up init2

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :decrecords
   resources :decpcbars
-  root "decpcbars#index"
+  root "decrecords#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
