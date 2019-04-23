b7d2fc77916555a64fdc7d2c63173b4a6ecf820f
zhaoruinan <zhaoruinan2010@gmail.com>
2019-04-23 19:52:23 +0900

    delete seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-(1..100).each do |i|
-  Decrecord.create!(title: "Lipsum Decrecord #{i}", body: %{
-      Nullam hendrerit iaculis sodales. Curabitur varius nibh arcu, id molestie nibh fermentum vitae. Cras quis semper dui. Cras porttitor urna sit amet risus vehicula tempor. Maecenas quis tempor ligula. Donec et nibh eu leo volutpat placerat. Fusce vulputate elit in nisi pretium, vel fermentum mi fermentum. Mauris scelerisque, lectus non luctus ultricies, urna eros tincidunt risus, at varius sapien diam id erat.
-  })
-end
+
