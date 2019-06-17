f0913dd0450efba45476756cd42b024a9a125056
zhaoruinan <zhaoruinan2010@gmail.com>
2019-06-18 03:51:32 +0900

    database repair

--- db/migrate/20190617160842_add_test_to_post.rb
+++
@@ -1,5 +0,0 @@
-class AddTestToPost < ActiveRecord::Migration[5.2]
-  def change
-    add_column :posts, :test, :var
-  end
-end
--- db/migrate/20190617161051_add_content_to_post.rb
+++
@@ -1,5 +0,0 @@
-class AddContentToPost < ActiveRecord::Migration[5.2]
-  def change
-    add_column :posts, :content, :content
-  end
-end
