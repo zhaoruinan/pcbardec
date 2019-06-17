1526a093a55e53b12098058f8665ec9e09331c4c
zhaoruinan <zhaoruinan2010@gmail.com>
2019-06-17 23:27:59 +0900

    add sign in

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_17_122451) do
+ActiveRecord::Schema.define(version: 2019_06_17_141717) do
 
   create_table "crono_jobs", force: :cascade do |t|
     t.string "job_id", null: false
@@ -44,6 +44,8 @@ ActiveRecord::Schema.define(version: 2019_06_17_122451) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
