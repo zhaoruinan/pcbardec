31f6fe04efbf07214c93e390473d73edcc6ab940
zhaoruinan <zhaoruinan2010@gmail.com>
2019-06-18 00:27:03 +0900

    add edit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,24 +1,93 @@
 class PostsController < ApplicationController
-	def show
-		@post = Post.find(params[:id])
-	end
-	def index
-		@posts = Post.all.order('created_at DESC')
-	end
-	
-	def new
-	end
-	def create
-		@post = Post.new(post_params)
-		@post.save
-		redirect_to @post
-	end
+	before_action :set_post, only: [:show, :edit, :update, :destroy]
+	before_action :authenticate_user!, except: [:index, :show]
+	before_action :authorized_user, only: [:edit, :update, :destroy]
 
-	def edit
-	end
+  # GET /links
+  # GET /links.json
+  def index
+    @posts = Post.all.order('created_at DESC')
+  end
 
-private
-		def post_params
-			params.require(:post).permit(:title, :body)
-		end
+  # GET /links/1
+  # GET /links/1.json
+  def show
+  end
+
+  # GET /links/new
+  def new
+    @post = current_user.posts.build
+  end
+
+  # GET /links/1/edit
+  def edit
+  end
+
+  # POST /links
+  # POST /links.json
+  def create
+    @post = current_user.posts.build(post_params)
+
+    respond_to do |format|
+      if @post.save
+        format.html { redirect_to @post, notice: 'post was successfully created.' }
+        format.json { render :show, status: :created, location: @post }
+      else
+        format.html { render :new }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /links/1
+  # PATCH/PUT /links/1.json
+  def update
+    respond_to do |format|
+      if @post.update(post_params)
+        format.html { redirect_to @post, notice: 'Link was successfully updated.' }
+        format.json { render :show, status: :ok, location: @post }
+      else
+        format.html { render :edit }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /links/1
+  # DELETE /links/1.json
+  def destroy
+    @post.destroy
+    respond_to do |format|
+      format.html { redirect_to posts_url, notice: 'post was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  def upvote
+    @post = Post.find(params[:id])
+    @post.upvote_by current_user
+    redirect_to :back
+  end
+
+  def downvote
+    @post = Post.find(params[:id])
+    @post.downvote_from current_user
+    redirect_to :back
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_post
+      @post = Post.find(params[:id])
+    end
+
+    def authorized_user
+      @post = current_user.posts.find_by(id: params[:id])
+      redirect_to posts_path, notice: "Not authorized to edit this post" if @post.nil?
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def post_params
+      params.require(:post).permit(:title, :body)
+    end
 end
